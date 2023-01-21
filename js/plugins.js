4b4458835b56cfcdddc65dbeadf3037250359047
Bashar-Saad-95 <basharsaad697@gmail.com>
2023-01-21 09:52:07 +0200

    Upload The Project Final

---
+++ js/plugins.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.carousel').carousel ({
+        interval:8000
+    });
+
+    // Show color option div when click on the gear
+    $(".check-color").click(function(){
+        $(".color-option").fadeToggle();
+    });
+
+    // Change Theam Color On Click
+    
+});
+
+
